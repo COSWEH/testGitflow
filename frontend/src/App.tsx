1ae52afdaac711afca597124ae1a61e5572d4eb3
COSWEH <ytoap0428@gmail.com>
2025-09-10 09:28:18 +0800

    feat: setup basic structure with shadcn installed

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <h2 className="text-2xl font-bold text-red-500 mb-10">Initial Setup</h2>
+      <Button variant="destructive">Click me</Button>
+    </div>
+  );
+}
+
+export default App;
